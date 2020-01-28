5b12e3db24ba57f0fb878fbf6c53f325098c6eec
Kyle Johnson <kyleaj@princeton.edu>
2020-01-28 15:57:39 -0500

    Animations + UI, pt. 1

--- iBeaconSimulateDemo/MainPage.xaml.cs
+++ iBeaconSimulateDemo/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -11,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -25,6 +27,47 @@ namespace iBeaconSimulateDemo
         public MainPage()
         {
             this.InitializeComponent();
+
+            // Keep Broadcasting animation large
+            SizeChanged += MainPage_SizeChanged;
+
+            // Transition smoothly between states
+            TransitionToBroad.Completed += ContinueWithBroadcast;
+            TransitionToStarting.Completed += ContinueWithStarting;
+            TransitionToError.Completed += ContinueWithError;
+        }
+
+        private void MainPage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            double radius = Math.Sqrt(Math.Pow(e.NewSize.Width, 2) + Math.Pow(e.NewSize.Height, 2));
+            BroadWidth.To = radius/100;
+            BroadHeight.To = radius/100;
+        }
+
+        private void Toggle_Toggled(object sender, RoutedEventArgs e)
+        {
+            if (Toggle.IsOn)
+            {
+                TransitionToStarting.Begin();
+            } else
+            {
+                TransitionToError.Begin();
+            }
+        }
+
+        private void ContinueWithBroadcast(object sender, object e)
+        {
+            BroadcastingAnimation.Begin();
+        }
+
+        private void ContinueWithStarting(object sender, object e)
+        {
+            StartingAnimation.Begin();
+        }
+
+        private void ContinueWithError(object sender, object e)
+        {
+            ErrorAnimation.Begin();
         }
     }
 }
